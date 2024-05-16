ad5c2ef57da73d35747c88a376cc89c61cdef93f
Hailey Verdone <haileyverdone@gmail.com>
2024-05-15 22:56:52 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './componets/Header';
 
 
 function App() {
   return (
     <div className="App">
-      <header />
+      <Header />
       <Introduction />
       <Skills />
       <Projects />
