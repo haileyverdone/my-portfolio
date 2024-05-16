6928d5942e08cb0f7882ec3c104e131f6d4ec888
Hailey Verdone <haileyverdone@gmail.com>
2024-05-15 22:21:00 -0700

    Intialize Project

---
+++ src/componets/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Header = () => (
+    <header>
+        <h1> My Portfolio </h1>
+        <nav>
+            <u1>
+            <li><a href="#introduction">Introduction</a></li>
+            <li><a href="skills">Skills</a></li>
+            <li><a href="projects">Projects</a></li>
+            <li><a href="contact">Contact</a></li>
+            </u1>
+        </nav>
+    </header>
+);
+
+export default Header;
